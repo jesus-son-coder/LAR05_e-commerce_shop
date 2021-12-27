4adad28a531f6af3bba0e03b95050a37e5282056
hseka <hseka@lescollectionneurs.com>
2021-12-27 21:00:41 +0100

    Ajustements Style/Css et autres correctifs #2

--- ecom/resources/views/admin/product/index.blade.php
+++ ecom/resources/views/admin/product/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.master')
 
 @section('content')
-
+    <div class="container-fluid product-list-container">
     @forelse($products as $product)
         <li>
             <h4>Nom du Produit: {{ $product->pro_name }}</h4>
@@ -14,4 +14,5 @@
     @empty
         <h3>Aucun produit </h3>
     @endforelse
+    </div>
 @endsection
